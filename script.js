f900e100284df1b0a4ab0f70c3ee5afe6331e17f
Isaias Gutierrez <isaiascruz@comunidad.unam.mx>
2021-10-24 17:18:09 -0500

    Basic structure and style finished

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
-ScrollReveal().reveal('.show', { enter: 'right',
-    wait: '1s',
-    move: '20px' });
+document.addEventListener('DOMContentLoaded', () => {
+  ScrollReveal().reveal('.show', { enter: 'right',
+      wait: '1s',
+      move: '20px' });
 
-ScrollReveal().reveal('.show1', { enter: 'right',
-    wait: '0.8s',
-    move: '20px' });
+  ScrollReveal().reveal('.show1', { enter: 'right',
+      wait: '0.8s',
+      move: '20px' });
 
-/*var config = {
-//    enter: 'right',
-    wait: '0.5s',
-    move: '20px'
-}
+  /*var config = {
+  //    enter: 'right',
+      wait: '0.5s',
+      move: '20px'
+  }
 
-new scrollReveal(config);*/
+  new scrollReveal(config);*/
 
 
-// Progress bar 
-let processScroll = () => {
-  let docElem = document.documentElement, 
-    docBody = document.body,
-    scrollTop = docElem['scrollTop'] || docBody['scrollTop'],
-      scrollBottom = (docElem['scrollHeight'] || docBody['scrollHeight']) - window.innerHeight,
-    scrollPercent = scrollTop / scrollBottom * 100 + '%';
-  
-  // console.log(scrollTop + ' / ' + scrollBottom + ' / ' + scrollPercent);
-  
-    document.getElementById("progress-bar").style.setProperty("--scrollAmount", scrollPercent); 
-}
+  // Progress bar 
+  let processScroll = () => {
+    let docElem = document.documentElement, 
+      docBody = document.body,
+      scrollTop = docElem['scrollTop'] || docBody['scrollTop'],
+        scrollBottom = (docElem['scrollHeight'] || docBody['scrollHeight']) - window.innerHeight,
+      scrollPercent = scrollTop / scrollBottom * 100 + '%';
+    
+    // console.log(scrollTop + ' / ' + scrollBottom + ' / ' + scrollPercent);
+    
+      document.getElementById("progress-bar").style.setProperty("--scrollAmount", scrollPercent); 
+  }
 
-document.addEventListener('scroll', processScroll);
+  document.addEventListener('scroll', processScroll);
+});
